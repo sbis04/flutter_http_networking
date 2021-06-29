df44acf02a49b76963da9bb41af44eaacbe420f1
Souvik Biswas <sbis1999@gmail.com>
2021-06-29 18:38:21 +0530

    Add home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_http_networking/models/post.dart';
+import 'package:flutter_http_networking/utils/http_client.dart';
 
 class HomePage extends StatelessWidget {
+  final PostClient _postClient = PostClient();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Networking'),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: FutureBuilder<Post?>(
+          future: _postClient.fetchPost(1),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              Post post = snapshot.data!;
+
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Title:\n${post.title}'),
+                  SizedBox(height: 8),
+                  Text('Body:\n${post.body}'),
+                ],
+              );
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
