d093c79488a20272246bd68a91bb5ed16f5b22e4
Souvik Biswas <sbis1999@gmail.com>
2021-06-29 18:38:06 +0530

    Add model class

---
+++ lib/models/post.dart
@@ -0,0 +1,27 @@
+class Post {
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  int userId;
+  int id;
+  String title;
+  String body;
+
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+        body: json["body"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+        "body": body,
+      };
+}
