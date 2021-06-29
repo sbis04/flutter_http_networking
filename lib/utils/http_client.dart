0bf7f20391514844f90df3acb3d41c7f091d5f34
Souvik Biswas <sbis1999@gmail.com>
2021-06-29 21:35:21 +0530

    Add post, update and delete

--- lib/utils/http_client.dart
+++ lib/utils/http_client.dart
@@ -4,17 +4,70 @@ import 'package:flutter_http_networking/models/post.dart';
 import 'package:http/http.dart' as http;
 
 class PostClient {
-  Future<Post?> fetchPost(int postId) async {
+  Future<Post> fetchPost(int postId) async {
     final url = Uri.parse("https://jsonplaceholder.typicode.com/posts/$postId");
     final response = await http.get(url);
-    Post retrievedPost;
 
     if (response.statusCode == 200) {
-      retrievedPost = Post.fromJson(jsonDecode(response.body));
+      return Post.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load post: $postId');
     }
+  }
 
-    return retrievedPost;
+  Future<Post> createPost(String title, String body) async {
+    final url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        'title': title,
+        'body': body,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return Post.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create post');
+    }
+  }
+
+  Future<Post> updatePost(String title, String body) async {
+    final url = Uri.parse("https://jsonplaceholder.typicode.com/posts/1");
+    final response = await http.put(
+      url,
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        'title': title,
+        'body': body,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return Post.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to update post');
+    }
+  }
+
+  Future<Post> deletePost(int postId) async {
+    final url = Uri.parse("https://jsonplaceholder.typicode.com/posts/$postId");
+    final response = await http.delete(
+      url,
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return Post.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to delete post: $postId');
+    }
   }
 }
